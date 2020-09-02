f85ebef85b56b4565c4db556f5e2f9d18504fd54
Trbuhozborac <nemanjapipi@hotmail.com>
2020-09-03 01:46:08 +0200

    Created View

---
+++ Dan_LIX_Nemanja_Pilipovic/MemoryGame/Card.cs
@@ -0,0 +1,26 @@
+﻿namespace MemoryGame
+{
+    class Card
+    {
+        #region Properties
+
+        public string Letter { get; set; }
+        public string Number { get; set; }
+        public bool  Guessed { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Card()
+        {
+
+        }
+        public Card(string letter)
+        {
+            Letter = letter;
+        }
+
+        #endregion
+    }
+}
